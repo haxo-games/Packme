2a843de01b61be95e76dee5accb90a26a3ebb03b
aXXo-dev <hunters12@protonmail.com>
2025-07-14 09:15:20 -0400

    feat: Stopping for 2day. Added more file parsing

--- main.cpp
+++ main.cpp
@@ -21,7 +21,7 @@ int main(int argc, char** argv)
     }
 
     auto input_path{ Arguments::getValue<std::string>(arguments, "-i").value() };
-    std::ifstream input_file(input_path.c_str(), std::ios::binary);
+    std::ifstream input_file(input_path, std::ios::binary);
 
     if (!input_file.is_open())
     {
@@ -29,8 +29,24 @@ int main(int argc, char** argv)
         return 2;
     }
 
-    IMAGE_DOS_HEADER dos_header{};
+    IMAGE_DOS_HEADER dos_header;
     input_file.read(reinterpret_cast<char*>(&dos_header), sizeof(dos_header));
+
+    if (dos_header.e_magic != IMAGE_DOS_SIGNATURE)
+    {
+        std::cerr << "[x] Invalid DOS signature" << std::endl;
+        return 3;
+    }
+
+    input_file.seekg(dos_header.e_lfanew + 4); // skip the signature (4)
+    if (!input_file.good()) 
+    {
+        std::cerr << "[x] Failed to seek to PE header" << std::endl;
+        return 4;
+    }
+
+    IMAGE_FILE_HEADER file_header;
+    input_file.read(reinterpret_cast<char*>(&file_header), sizeof(file_header));
 
 	return 0;
 }
