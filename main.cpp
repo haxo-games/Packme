cd6722a525710cd31fa948a726c469260f84c1b3
aXXo-dev <hunters12@protonmail.com>
2025-07-14 09:11:41 -0400

    feat: Read DOS header

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <fstream>
+
+#include <windows.h>
 
 #include "arguments.h"
 
@@ -6,7 +9,7 @@ int main(int argc, char** argv)
 {
     Arguments::Map arguments = 
     {
-        {"-i", Arguments::Argument(Arguments::Type::STRING)},
+        {"-i", Arguments::Argument(Arguments::Type::STRING, true)},
     };
 
     int return_code{ Arguments::parse(argc, argv, arguments) };
@@ -16,6 +19,18 @@ int main(int argc, char** argv)
         std::cerr << "[x] Failed to parse arguments with error code " << return_code << std::endl;
         return 1;
     }
+
+    auto input_path{ Arguments::getValue<std::string>(arguments, "-i").value() };
+    std::ifstream input_file(input_path.c_str(), std::ios::binary);
+
+    if (!input_file.is_open())
+    {
+        std::cerr << "[x] Failed to open file " << input_path << std::endl;
+        return 2;
+    }
+
+    IMAGE_DOS_HEADER dos_header{};
+    input_file.read(reinterpret_cast<char*>(&dos_header), sizeof(dos_header));
 
 	return 0;
 }
