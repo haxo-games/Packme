f8fdb95cafeb33d3ca755fcbd8a132541b5888fe
aXXo-dev <hunters12@protonmail.com>
2025-07-23 10:43:21 -0400

    i give up for today

--- Stub/main.cpp
+++ Stub/main.cpp
@@ -12,13 +12,13 @@ int main()
 
 	HMODULE h_module{ GetModuleHandle(0) };
 	if (!h_module)
-		ExitProcess(1);
+		return 1;
 
 	uint8_t* p_packed{ reinterpret_cast<uint8_t*>(h_module) + stub_config.packed_data_rva };
 
 	void* p_unpacked{ VirtualAlloc(nullptr, stub_config.original_data_size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE) };
 	if (!p_unpacked)
-		ExitProcess(2);
+		return 2;
 
 	uLongf dest_len{ stub_config.original_data_size };
 	int return_code{ uncompress(static_cast<Bytef*>(p_unpacked), &dest_len, p_packed, stub_config.packed_data_size) };
@@ -26,7 +26,7 @@ int main()
 	if (return_code != Z_OK || dest_len != stub_config.original_data_size)
 	{
 		VirtualFree(p_unpacked, 0, MEM_RELEASE);
-		ExitProcess(3);
+		return 3;
 	}
 
 	IMAGE_DOS_HEADER* p_dos_header{ reinterpret_cast<IMAGE_DOS_HEADER*>(p_unpacked) };
@@ -37,7 +37,7 @@ int main()
 	if (delta_address)
 	{
 		if (!p_nt_headers->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_BASERELOC].Size)
-			ExitProcess(4);
+			return 4;
 	}
 
 	VirtualFree(p_unpacked, 0, MEM_RELEASE);
